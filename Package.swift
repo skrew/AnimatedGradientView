f356373cafde00cfe113fc300264da35314f517d
Eric <eric@skrobs.com>
2021-05-04 02:36:40 +0200

    WIP

--- Package.swift
+++ Package.swift
@@ -1,13 +1,14 @@
-// swift-tools-version:5.0
- import PackageDescription
+// swift-tools-version:5.4
+import PackageDescription
 
- let package = Package(
-     name: "AnimatedGradientView",
-     platforms: [.iOS(.v9)],
-     products: [
-         .library(name: "AnimatedGradientView", targets: ["AnimatedGradientView"])
-     ],
-     targets: [
-         .target(name: "AnimatedGradientView", path: "AnimatedGradientView/Classes")
-     ]
- )
+let package = Package(
+    name: "AnimatedGradientView",
+    platforms: [.iOS(.v9), .tvOS(.v13)],
+    products: [
+        .library(name: "AnimatedGradientView", targets: ["AnimatedGradientView"])
+    ],
+    targets: [
+        .target(name: "AnimatedGradientView", path: "AnimatedGradientView/Classes")
+    ],
+    swiftLanguageVersions: [.v5]
+)
